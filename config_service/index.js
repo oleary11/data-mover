2b8000a8eb40a7a57f89f02643c06c8a3385665e
Sreeram Ganesan <sregsn@Sreerams-MacBook-Pro.local>
2023-04-04 03:16:44 -0600

    feat: Adding PUT and DELETE endpoints for Source

--- config_service/index.js
+++ config_service/index.js
@@ -34,7 +34,18 @@ app.get('/source', (req, res) => {
     })
     .catch((err) => {
         console.log(err);
-        res.json({"message" : "Error fetching sources!"});
+        res.json({"message" : "Error fetching sources."});
+    })
+});
+
+app.get('/source/:id', (req, res) => {
+    Source.findOne({"SourceId" : req.params.id})
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({"message" : "Error fetching source."});
     })
 });
 
@@ -43,6 +54,32 @@ app.post('/source', jsonParser, (req, res) => {
     new_source.save().then(val => {
         res.json({ message: "Source added successfully", data: val })
       });
+});
+
+app.put('/source/:id', jsonParser, (req, res) => {
+    const new_source = new Source(req.body);
+    Source.updateOne({ "SourceId" : req.params.id}, {
+        $set: {
+            SourceType : new_source.SourceType,
+            ClientId : new_source.ClientId,
+            ClientSecret : new_source.ClientSecret,
+            RefreshToken : new_source.RefreshToken
+        }
+    }).then(val => {
+        res.json({ message: "Source updated successfully", data: val })
+      });
+  });
+
+app.delete('/source/:id', jsonParser, (req, res) => {
+    
+    Source.deleteOne({ "SourceId" : req.params.id})
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({"message" : "Error deleting source."});
+    })
 });
 
 // Listener
