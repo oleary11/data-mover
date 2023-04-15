9d6c3220d63e9de65b5b35c38afbdca0b88f10ae
Sreeram Ganesan <sregsn@Sreerams-MacBook-Pro.local>
2023-04-15 13:10:34 -0600

    refactor: Adding methods for CORS

--- config_service/index.js
+++ config_service/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const Source = require('./models/Source')
 const Destination = require('./models/Destination');
+const Connection = require('./models/Connection');
 
 // Constants
 const app = express();
@@ -141,9 +142,64 @@ app.delete('/destination/:id', jsonParser, (req,res) => {
     })
 });
 
+app.get('/connection', jsonParser, (req, res) => {
+    Connection.find({})
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.json({"message" : "Error fetching connections."});
+    })
+});
+
+app.get('/connection/:id', jsonParser, (req, res) => {
+    Connection.findOne({"ConnectionId" : req.params.id})
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.json({"message" : "Error fetching connection."});
+    })
+});
+
+app.post('/connection', jsonParser, (req, res) => {
+    const new_connection = new Connection(req.body);
+    new_connection.save().then(val => {
+        res.json({ message: "Connection added successfully", data: val })
+      });
+});
+
+app.put('/connection/:id', jsonParser, (req,res) => {
+    const new_connection = new Connection(req.body);
+    Connection.updateOne({"ConnectionId" : req.params.id}, {
+        $set: {
+            SourceId: new_connection.SourceId,
+            DestinationId: new_connection.DestinationId
+        }
+    })
+    .then(val => {
+        res.json({message : "Connection updated successfully", data: val})
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({"message" : "Error updating Connection."});
+    });
+});
+
+app.delete('/connection/:id', jsonParser, (req,res) => {
+    Connection.deleteOne({"ConnectionId" : req.params.id})
+    .then((data) => {
+        res.json(data)
+    })
+    .catch((err) => {
+        res.json({"message" : "Error deleting connection."});
+    })
+});
+
 // Middleware
 app.use(cors({
-    origin: '*'
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
 // Listener
