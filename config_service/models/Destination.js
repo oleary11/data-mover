fc2fba9a8cb4e85d549c61bc842ebc25695e50eb
Sreeram Ganesan <sreeram2910@gmail.com>
2023-05-02 22:59:40 -0600

    fix: updating destination model

--- config_service/models/Destination.js
+++ config_service/models/Destination.js
@@ -4,11 +4,17 @@ const { Schema, model } = mongoose;
 const destinationSchema = new Schema({
   DestinationId: Number,
   DestinationType: String,
+  SheetId: String,
+  SheetName: String,
   Host: String,
   Port: String,
   DBName: String,
   User: String,
-  Pass: String
+  Pass: String,
+  DatasetId: Number,
+  ProjectId: String,
+  TableId: String,
+  key: String
 });
 
 module.exports = mongoose.model('Destination',destinationSchema)
